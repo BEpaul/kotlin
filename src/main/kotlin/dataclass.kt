036121c98458bbcd0813fe0c250f35a408a325b2
Minhyuk Kim <kim.minhyuk@mintech.kr>
2023-07-17 18:22:36 +0900

    코틀린 object, dataclass 문법

--- src/main/kotlin/dataclass.kt
+++ src/main/kotlin/dataclass.kt
@@ -1,2 +1,12 @@
-class dataclass {
+data class Ticket(val companyName: String, val name: String, var date: String, var seatNumber: Int)
+
+// toString(), hashCode(), equals(), copy()
+class TicketNormal(val companyName: String, val name: String, var date: String, var seatNumber: Int)
+
+fun main() {
+    val ticketA = Ticket("tway", "minhyukKim", "2023-08-25", 1)
+    val ticketB = TicketNormal("tway", "minhyukKim", "2023-08-25", 1)
+
+    println(ticketA)
+    println(ticketB)
 }
